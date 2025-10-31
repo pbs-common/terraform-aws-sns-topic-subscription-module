63792ea65823dc73a103c45cb15bf424c3433cb5
Dragos Andreescu <dragos.andreescu@3pillarglobal.com>
2025-10-31 09:45:17 +0200

    CAT-25883 - update terraform / provider / go versions, update go dependencies, fix tests

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,27 +1,29 @@
 module "topic" {
-  source = "github.com/pbs/terraform-aws-sns-topic-module?ref=0.0.1"
+  source = "github.com/pbs/terraform-aws-sns-topic-module?ref=1.0.0"
 
   organization = var.organization
   environment  = var.environment
   product      = var.product
   repo         = var.repo
+  owner        = var.owner
 }
 
 module "lambda" {
-  source = "github.com/pbs/terraform-aws-lambda-module?ref=1.3.2"
+  source = "github.com/pbs/terraform-aws-lambda-module?ref=2.0.0"
 
   handler  = "main.lambda_handler"
   filename = "./artifacts/deploy.zip"
-  runtime  = "python3.9"
+  runtime  = "python3.13"
 
   organization = var.organization
   environment  = var.environment
   product      = var.product
   repo         = var.repo
+  owner        = var.owner
 }
 
 module "lambda_permission" {
-  source = "github.com/pbs/terraform-aws-lambda-permission-module?ref=0.0.1"
+  source = "github.com/pbs/terraform-aws-lambda-permission-module?ref=1.0.0"
 
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
